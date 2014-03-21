c638781ec61a1e594fcb3c149656eb9b8a1d6225
Brittany Mazza & Ivan Sued <pair+LadyMozzarella.thefenry@devbootcamp.com>
2014-03-20 20:08:48 -0700

    Add session controller rspec test outline

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,17 +2,13 @@ require 'spec_helper'
 
 describe do SessionsController
   let!(:user) { :user }
+
   describe "#new" do
-    it "should be successful" do
-      get :new
-      expect(response).to be_success
-    end
+    it "should be successful"
   end
 
   describe "#create" do
-    it "should initialize a new session" do
-
-    end
+    it "should initialize a new session"
   end
 
   describe "#destroy" do
