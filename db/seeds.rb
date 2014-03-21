c3a4a22aa964cbd3a9e597be3eb719a3239fc509
Brittany Mazza & Ivan Sued <pair+LadyMozzarella.thefenry@devbootcamp.com>
2014-03-20 19:19:22 -0700

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ivan = User.create(email: "ivan@betterbet.com", password_digest: "ivan", name: "Ivan", bio: Faker::Lorem.paragraph)
+brittany = User.create(email: "brittany@betterbet.com", password_digest: "Brittany", name: "Brittany", bio: Faker::Lorem.paragraph)
+jose = User.create(email: "jose@betterbet.com", password_digest: "jose", name: "Jose", bio: Faker::Lorem.paragraph)
+irene = User.create(email: "irene@betterbet.com", password_digest: "irene", name: "Irene", bio: Faker::Lorem.paragraph)
+anne = User.create(email: "anne@betterbet.com", password_digest: "anne", name: "Anne", bio: Faker::Lorem.paragraph)
+
