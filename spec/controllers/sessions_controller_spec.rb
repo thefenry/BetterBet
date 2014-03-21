1a39715006ca4035e1a205426e170bdd368fa558
Brittany Mazza & Ivan Sued <pair+LadyMozzarella.thefenry@devbootcamp.com>
2014-03-20 20:03:53 -0700

    WIP - Session rspec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,1 +1,21 @@
 require 'spec_helper'
+
+describe do SessionsController
+  let!(:user) { :user }
+  describe "#new" do
+    it "should be successful" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+  describe "#create" do
+    it "should initialize a new session" do
+
+    end
+  end
+
+  describe "#destroy" do
+    it "should clear a session"
+  end
+end
